15aa6e1e6a227690bdc8edb5cc3631e99fc6e4e5
nkhachan <noopur.khachane@gmail.com>
2018-12-13 11:44:16 -0800

    add sample printing

--- src/src/PrintQRCode.py
+++ src/src/PrintQRCode.py
@@ -15,5 +15,7 @@ qr.make(fit=True)
 img = qr.make_image(fill_color="black", back_color="white")
 
 printer = Adafruit_Thermal("/dev/ttyUSB0", 9600, timeout=3000)
-printer.printImage(img, True)
+from sampleqr import *
+printer.printBitmap(width, height, data)
+#printer.printImage(img, True)
 printer.feed(2)
